7d66b6215eb41a406002b2788c84d871bdef53af
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-24 10:38:24 +0500

    Add files via upload

---
+++ piperrrrrr.js
@@ -0,0 +1,34 @@
+let num = +prompt('7 gacha son kiriting ✨')
+
+switch (num) {
+    case 1:
+        alert('Dushanba 🎭')
+        break;
+
+    case 2:
+        alert('Seshanba 🎭')
+        break;
+
+    case 3:
+        alert('Chorshanba 🎭')
+        break;
+
+    case 4:
+        alert('Payshanba 🎭')
+        break;
+
+    case 5:
+        alert('Juma 🎭')
+        break;
+
+    case 6:
+        alert('Shanba 🎭')
+        break;
+
+    case 7:
+        alert('Yakshanba 🎭')
+        break;
+    default:
+        alert('Bir haftada 7 kun bor!!! Ikkichi')
+        break;
+}
